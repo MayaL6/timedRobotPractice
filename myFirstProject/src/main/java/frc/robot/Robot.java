4c82488bd131d78e1cadacac497eff575dbcfd97
Maya Lev <maya.lev.2009@gmail.com>
2024-07-09 14:03:19 +0300

    feat(Robot.java, vendordeps): Import Pheonix5 and Pheonix6, Addmotor movement based on controller.

--- myFirstProject/src/main/java/frc/robot/Robot.java
+++ myFirstProject/src/main/java/frc/robot/Robot.java
@@ -4,6 +4,10 @@
 
 package frc.robot;
 
+import com.ctre.phoenix.motorcontrol.ControlMode;
+import com.ctre.phoenix.motorcontrol.can.TalonSRX;
+
+import edu.wpi.first.wpilibj.Joystick;
 import edu.wpi.first.wpilibj.TimedRobot;
 import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
@@ -15,10 +19,8 @@ import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
  * project.
  */
 public class Robot extends TimedRobot {
-  private static final String kDefaultAuto = "Default";
-  private static final String kCustomAuto = "My Auto";
-  private String m_autoSelected;
-  private final SendableChooser<String> m_chooser = new SendableChooser<>();
+  private Joystick joystick;
+  private TalonSRX motor;
 
   /**
    * This function is run when the robot is first started up and should be used for any
@@ -26,9 +28,9 @@ public class Robot extends TimedRobot {
    */
   @Override
   public void robotInit() {
-    m_chooser.setDefaultOption("Default Auto", kDefaultAuto);
-    m_chooser.addOption("My Auto", kCustomAuto);
-    SmartDashboard.putData("Auto choices", m_chooser);
+    joystick = new Joystick(0);
+    motor = new TalonSRX(1);
+
   }
 
   /**
@@ -53,32 +55,31 @@ public class Robot extends TimedRobot {
    */
   @Override
   public void autonomousInit() {
-    m_autoSelected = m_chooser.getSelected();
-    // m_autoSelected = SmartDashboard.getString("Auto Selector", kDefaultAuto);
-    System.out.println("Auto selected: " + m_autoSelected);
+
   }
 
   /** This function is called periodically during autonomous. */
   @Override
   public void autonomousPeriodic() {
-    switch (m_autoSelected) {
-      case kCustomAuto:
-        // Put custom auto code here
-        break;
-      case kDefaultAuto:
-      default:
-        // Put default auto code here
-        break;
-    }
+
   }
 
   /** This function is called once when teleop is enabled. */
   @Override
-  public void teleopInit() {}
-
+  public void teleopInit() {
+  }
+    
   /** This function is called periodically during operator control. */
   @Override
-  public void teleopPeriodic() {}
+  public void teleopPeriodic() {
+    if(joystick.getRawButton(1)) {
+      motor.set(ControlMode.PercentOutput, 0.3);
+    }
+
+    if(joystick.getRawButton(2)) {
+      motor.set(ControlMode.PercentOutput, 0.3);
+    }
+  }
 
   /** This function is called once when the robot is disabled. */
   @Override
